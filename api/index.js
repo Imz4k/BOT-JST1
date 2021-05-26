549aeb757d748196e2ccba6738d548e3b61ce7d6
Imz4k <84834826+Imz4k@users.noreply.github.com>
2021-05-26 20:08:55 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -4,11 +4,10 @@ var r = express.Router();
 // load pre-trained model
 const model = require('./sdk/model.js');
 
-// Bots Setting
+// Bot Setting
 const TelegramBot = require('node-telegram-bot-api');
 const token = '1812069205:AAGRYRfWv6HOqDWdJTqxB786nNftAFmGI80'
 const bot = new TelegramBot(token, {polling: true});
-
 
 // Main menu bots
 bot.onText(/\/start/, (msg) => { 
@@ -55,7 +54,6 @@ bot.on('message', (msg) => {
         state = 0
     }
 });
-
 // routers
 r.get('/prediction/:i/:r', function(req, res, next) {    
     model.predict(
