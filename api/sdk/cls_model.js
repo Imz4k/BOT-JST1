833016acc8349823373670360eea6b430fb99ff6
Imz4k <84834826+Imz4k@users.noreply.github.com>
2021-06-02 00:25:51 +0700

    Update cls_model.js

--- api/sdk/cls_model.js
+++ api/sdk/cls_model.js
@@ -31,7 +31,7 @@ async function classify(data){
 
     try{
         // path load in public access => github
-        const path = 'https://raw.githubusercontent.com/Imz4k/BOT-JST1/main/public/ex_model/model.json';
+        const path = 'https://raw.githubusercontent.com/Imz4k/BOT-JST1/main/public/cls_model/model.json';
         const model = await tf.loadGraphModel(path);
         
         predict = model.predict(
